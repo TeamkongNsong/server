481524353d8983a38debe2e2b1eced9e759b33a4
Yungoo <ohsory1324@naver.com>
2017-02-03 14:57:47 +0900

    init

---
+++ mysql/knex.js
@@ -0,0 +1,12 @@
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : 'wikius.cm5qkmkpq74f.ap-northeast-2.rds.amazonaws.com',
+    port : '3306',
+    user : 'kongNsong',
+    password : '9294duo!',
+    database : 'yoongoodb'
+  }
+});
+
+exports.knex = knex;
